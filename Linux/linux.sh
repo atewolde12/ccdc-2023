40249efbb6a30aeab8f267b4dd20a015a5cd063f
RyanH7279 <snoopy8911@hotmail.com>
2023-03-03 03:16:37 -0700

    Add files via upload

--- Linux/linux.sh
+++ Linux/linux.sh
@@ -37,24 +37,24 @@ echo "
 8.) chmod critical
 9.) Exit"
 read input
-if ["$input" == "1"]; then
+if [$input -eq 1]; then
     echo 1
     packageSelect
-elif ["$input" == "2"]; then
+elif [$input -eq 2]; then
     nologin
-elif ["$input" == "3"]; then
+elif [$input -eq 3]; then
     chgpasswd
-elif ["$input" == "4"]; then
+elif [$input -eq 4]; then
     SSHDconf
-elif ["$input" == "5"]; then
+elif [$input -eq 5]; then
     UFWconf
-elif ["$input" == "6"]; then
+elif [$input -eq 6]; then
     updategrade
-elif ["$input" == "7"]; then
+elif [$input -eq 7]; then
     resolv 
-elif ["$input" == "8"]; then
+elif [$input -eq 8]; then
     chmodcrit
-elif ["$input" == "9"]; then
+elif [$input -eq 9]; then
     exit
 else
     start
